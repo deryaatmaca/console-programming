390ae5816741930c87e31775c557d2f0fc63a7af
Derya Atmaca <derya.atmaca@outlook.com>
2022-02-27 17:31:55 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin:");
+            string name= Console.ReadLine();
+            Console.WriteLine("Soyadınızı girin:");
+             string surname= Console.ReadLine();
+             Console.WriteLine("Merhaba" + " " + name +  surname);
+        }
+    }
+}
+ 
